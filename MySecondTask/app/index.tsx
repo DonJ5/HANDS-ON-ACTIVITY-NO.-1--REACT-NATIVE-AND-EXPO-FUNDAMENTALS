d103c1b199fddc741958c789b20c3e6a5587e03d
DonJ5 <donjiecastino0@gmail.com>
2025-06-24 11:27:02 +0800

    Initial commit

---
+++ MySecondTask/app/index.tsx
@@ -0,0 +1,31 @@
+import { Image } from "expo-image";
+import { Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Image source={require('@/assets/images/MarkZuckerberg.jpg')} style = {styles.image}></Image>
+      <Text style = {styles.txt}>Mark Zuckerberg</Text>
+      <Text style = {styles.txt2}>CEO Meta</Text>
+    </View>
+  );
+}
+const styles ={
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+},
+txt: {
+  fontWeight: "bold",
+  fontSize: '24px',
+},
+txt2: {
+  fontSize: '18px'
+},
+image: {
+  width: 250,
+  height: 300,
+  borderRadius:20
+}}
---
+++ MyThirdTask/app/index.tsx
@@ -0,0 +1,42 @@
+import { Image } from "expo-image";
+import { Text, View } from "react-native";
+export default function Index() {
+  return (
+    <View style = {styles.container}>
+
+      <Text style = {styles.txt}>COLLEGE OF COMPUTING AND INFORMATION SCIENCES</Text>
+      
+      <Image source={require('@/assets/images/ccislogo.png')} style = {styles.image}></Image>
+      
+      <Text style = {styles.txt2}>Developed by</Text>
+      <Text style = {styles.txt3}>Donjie J. Castino</Text>
+    </View>
+  );
+}
+const styles = {
+container: {
+  flex: 1,
+  paddingTop: 150,
+  alignItems: "center",
+  background: 'white'
+},
+txt: {
+  
+  fontWeight: "bold",
+  fontSize: "25px",
+  textAlign: "center"
+},
+image: {
+  
+  width:  400,
+  height: 400,
+},
+txt2: {
+  paddingTop: 140,
+  fontSize: '16px',
+},
+txt3: {
+  fontSize: '20px',
+  fontWeight: 'bold'
+}
+}
